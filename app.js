06ffc346ca452187c31f8c865f7e312afe1af07e
Matúš <kollarm9@gmail.com>
2025-10-26 10:23:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.PG_URI,
+});
+
+var app = express();
+
+app.get('/health/db', async (req, res) => {
+  try {
+    await pool.query('SELECT 1'); // lightweight, doesn’t modify anything
+    res.send('DB connection OK');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('DB not connected');
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+module.exports = app;
